eb625f24e5cac3204fd2a265fa2231237d3f3e9b
XJiaoLi <xuejioali521@gmail.com>
2023-07-05 17:35:49 +0800

    modified:   Dockerfile 	modified:   README.md 	modified:   crazy_functional.py 	modified:   "crazy_functions/Langchain\347\237\245\350\257\206\345\272\223.py" 	modified:   "crazy_functions/Latex\345\205\250\346\226\207\346\266\246\350\211\262.py" 	new file:   "crazy_functions/Latex\350\276\223\345\207\272PDF\347\273\223\346\236\234.py" 	modified:   crazy_functions/crazy_functions_test.py 	new file:   crazy_functions/latex_utils.py 	modified:   "crazy_functions/\346\225\260\345\255\246\345\212\250\347\224\273\347\224\237\346\210\220manim.py" 	new file:   docs/Dockerfile+NoLocal+Latex 	modified:   docs/translate_english.json 	modified:   toolbox.py 	modified:   version

--- crazy_functions/crazy_functions_test.py
+++ crazy_functions/crazy_functions_test.py
@@ -179,6 +179,29 @@ def test_Langchain知识库读取():
     for cookies, cb, hist, msg in silence_stdout(读取知识库作答)(txt, llm_kwargs, plugin_kwargs, chatbot, history, system_prompt, web_port):
         cli_printer.print(cb)   #  print(cb)
 
+def test_Latex():
+    from crazy_functions.Latex输出PDF结果 import Latex英文纠错加PDF对比, Latex翻译中文并重新编译PDF
+
+    # txt = r"https://arxiv.org/abs/1706.03762"
+    # txt = r"https://arxiv.org/abs/1902.03185"
+    # txt = r"https://arxiv.org/abs/2305.18290"
+    # txt = r"https://arxiv.org/abs/2305.17608"
+    # txt = r"https://arxiv.org/abs/2211.16068"                     #  ACE
+    # txt = r"C:\Users\x\arxiv_cache\2211.16068\workfolder"  #  ACE
+    txt = r"https://arxiv.org/abs/2002.09253"
+    for cookies, cb, hist, msg in (Latex翻译中文并重新编译PDF)(txt, llm_kwargs, plugin_kwargs, chatbot, history, system_prompt, web_port):
+        cli_printer.print(cb)   #  print(cb)
+
+
+
+    # txt = "2302.02948.tar"
+    # print(txt)
+    # main_tex, work_folder = Latex预处理(txt)
+    # print('main tex:', main_tex)
+    # res = 编译Latex(main_tex, work_folder)
+    # # for cookies, cb, hist, msg in silence_stdout(编译Latex)(txt, llm_kwargs, plugin_kwargs, chatbot, history, system_prompt, web_port):
+    #     cli_printer.print(cb)   #  print(cb)
+
 
 
 # test_解析一个Python项目()
@@ -192,8 +215,8 @@ def test_Langchain知识库读取():
 # test_联网回答问题()
 # test_解析ipynb文件()
 # test_数学动画生成manim()
-test_Langchain知识库()
+# test_Langchain知识库()
 # test_Langchain知识库读取()
-
+test_Latex()
 input("程序完成，回车退出。")
 print("退出。")
